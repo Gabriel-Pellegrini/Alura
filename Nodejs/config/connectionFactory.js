26f73efd2ed4d5a7952a52eb3eb81e53d17aa68e
Gabriel Pellegrini <gpellegrinimaciel96@gmail.com>
2018-07-22 00:59:06 -0300

    function wrapper added

--- Nodejs/config/connectionFactory.js
+++ Nodejs/config/connectionFactory.js
@@ -1,11 +1,11 @@
 var mysql = require('mysql');
 
-
+//Wrapper
 module.exports = function () {
     return connMySQL
 };
 
-var connMySQL = function () {
+function connMySQL() {
     console.log(' Connection with Database was estabilished')
     return mysql.createConnection({
         host: 'localhost',
