6f689f56fdf69eff49eecf8d9d02754a683631aa
Gabriel Pellegrini <gpellegrinimaciel96@gmail.com>
2018-07-22 00:39:57 -0300

    Ajusts on product route

--- Nodejs/app/routes/product.js
+++ Nodejs/app/routes/product.js
@@ -4,7 +4,6 @@ module.exports = function (application) {
     application.get("/", function (req, res) {
         
         var connection = application.config.connectionFactory();
-        // var connection = application.config.connectionFactory();
         connection.query("select * from livros",function (err,results) {
             res.render("produtos/lista",{lista:results});
         })
